3777ef53daca95e0b92e233813af71c8babdb3c7
hadron <keelanhanrahan12345@gmail.com>
2024-07-16 11:07:44 +0100

    Version.003
    Added code to kill orthogonal velocity using a variable called "Drift factor", which only allows whatever percentage of orthogonal velocity to be used. Need to add a cap to acceleration also.

--- TopDownCarController.cs
+++ TopDownCarController.cs
@@ -5,9 +5,11 @@ public class TopDownCarController : MonoBehaviour
 {
     public float accelerationFactor = 30.0f;
     public float turnFactor = 3.5f;
+    public float driftFactor = 0.95f;
     float rotationAngle = 0;
     float accelerationInput = 0;
     float steeringInput = 0;
+
 
 
     Rigidbody2D rb;
@@ -21,10 +23,31 @@ public class TopDownCarController : MonoBehaviour
     void FixedUpdate()
     {
         ApplyEngineForce();
+
+        KillOrthogonalVelocity();
+
         ApplySteering();
     }
 
-    void ApplyEngineForce(){
+    void KillOrthogonalVelocity(){
+        // gives a values for how much forward velocity a car has
+        Vector2 forwardVelocity = transform.up * Vector2.Dot(rb.velocity, transform.up);
+
+        // gives a values for how much right velocity a car has
+        Vector2 rightVelocity = transform.right * Vector2.Dot(rb.velocity, transform.right);
+
+        rb.velocity = forwardVelocity + rightVelocity * driftFactor;
+    }
+
+    void ApplyEngineForce()
+    {
+        //applying some resistance/drag when the user isnt accelerating to stop the car slowly
+        if (accelerationInput == 0){
+            rb.drag = Mathf.Lerp(rb.drag, 3.0f, Time.fixedDeltaTime * 3);
+        } else {
+            rb.drag = 0;
+        }
+
         Vector2 engineForceVector = transform.up * accelerationInput * accelerationFactor;
         rb.AddForce(engineForceVector, ForceMode2D.Force);
     }
